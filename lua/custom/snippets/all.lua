5c6367e372b6d5329d944e84c71139d9b333fcce
Jon Glueckstein <jglueckstein@gmail.com>
2023-11-29 16:55:02 -0500

    added functionality to allow custom snippets with luasnip and cmp

---
+++ lua/custom/snippets/all.lua
@@ -0,0 +1,20 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+
+return {
+    -- A snippet that expands the trigger "hi" into the string "Hello, world!".
+    require("luasnip").snippet(
+        { trig = "hi" },
+        { t("Hello, world!") }
+    ),
+
+    -- To return multiple snippets, use one `return` statement per snippet file
+    -- and return a table of Lua snippets.
+    require("luasnip").snippet(
+        { trig = "foo" },
+        { t("Another snippet.") }
+    ),
+}
