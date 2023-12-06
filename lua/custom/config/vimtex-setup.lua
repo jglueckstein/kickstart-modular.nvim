a1b05a2daab2d09ca14c7db00119610725b05326
Jon Glueckstein <jglueckstein@gmail.com>
2023-12-06 09:27:24 -0500

    vimtex config to use zathura

--- lua/custom/config/vimtex-setup.lua
+++ lua/custom/config/vimtex-setup.lua
@@ -3,3 +3,9 @@ vim.g['vimtex_syntax_enabled'] = 1
 
 -- Folding (defaults to disabled)
 vim.g['vimtex_fold_enabled'] = 1
+
+-- Use Zathura as the VimTeX PDF viewer
+vim.g['vimtex_view_method'] = 'zathura'
+
+-- Keymaps
+vim.keymap.set('n', '<localleader>v', '<Plug>(vimtex-view)')
