9bda7e1a4b109d744527c9a57c2ff025d9d691c0
Jon Glueckstein <jglueckstein@gmail.com>
2024-02-28 10:05:36 -0500

    keymap: <ESC> to exit terminal mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -15,23 +15,23 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open float
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- repurpose the q key for closing a window
-vim.keymap.set("n", "Q", "q", { silent = true, desc = 'start recording a macro'})                     -- Q instead of q to start recording a macro
-vim.keymap.set("n", "gQ", "@q", { silent = true, desc = 'run a macro called "q"'})                   -- gQ instead of @q to run a macro called "q"
-vim.keymap.set("n", "q", ":pclose!<CR>:quit<CR>", { silent = true, desc = 'fast quitting current window'}) -- q provides fast quitting of window (and nvim, if last window)
+vim.keymap.set("n", "Q", "q", { silent = true, desc = 'start recording a macro' })                         -- Q instead of q to start recording a macro
+vim.keymap.set("n", "gQ", "@q", { silent = true, desc = 'run a macro called "q"' })                        -- gQ instead of @q to run a macro called "q"
+vim.keymap.set("n", "q", ":pclose!<CR>:quit<CR>", { silent = true, desc = 'fast quitting current window' }) -- q provides fast quitting of window (and nvim, if last window)
 
 -- fast saving from all modes
-vim.keymap.set("n", "<Leader>w", ":write<CR>", { silent = true, desc = 'Save buffer'})    -- normal mode
-vim.keymap.set("x", "<Leader>w", "<Esc>:write<CR>", { silent = true, desc = 'Save buffer'})   -- visual mode
-vim.keymap.set("n", "<C-s>", ":<C-u>write<CR>", { silent = true, desc = 'Save buffer'})   -- normal mode
-vim.keymap.set("x", "<C-s>", ":<C-u>write<CR>", { silent = true, desc = 'Save buffer'})   -- visual mode
-vim.keymap.set("c", "<C-s>", "<C-u>write<CR>", { silent = true, desc = 'Save buffer'})    -- command-line mode
+vim.keymap.set("n", "<Leader>w", ":write<CR>", { silent = true, desc = 'Save buffer' })     -- normal mode
+vim.keymap.set("x", "<Leader>w", "<Esc>:write<CR>", { silent = true, desc = 'Save buffer' }) -- visual mode
+vim.keymap.set("n", "<C-s>", ":<C-u>write<CR>", { silent = true, desc = 'Save buffer' })    -- normal mode
+vim.keymap.set("x", "<C-s>", ":<C-u>write<CR>", { silent = true, desc = 'Save buffer' })    -- visual mode
+vim.keymap.set("c", "<C-s>", "<C-u>write<CR>", { silent = true, desc = 'Save buffer' })     -- command-line mode
 
 -- fast save and quit all windows with CTRL-q
-vim.keymap.set("n", "<C-q>", "<CMD>wqa<CR>", { silent = true, desc = 'save and quit all windows'})
+vim.keymap.set("n", "<C-q>", "<CMD>wqa<CR>", { silent = true, desc = 'save and quit all windows' })
 
 -- fast exit from insert mode
-vim.keymap.set("i", "jk", "<ESC>", { silent = true, desc = 'fast exit from insert mode'})
-vim.keymap.set("i", "kj", "<ESC>", { silent = true, desc = 'fast exit from insert mode'})
+vim.keymap.set("i", "jk", "<ESC>", { silent = true, desc = 'fast exit from insert mode' })
+vim.keymap.set("i", "kj", "<ESC>", { silent = true, desc = 'fast exit from insert mode' })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -43,5 +43,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+-- fast escape from terminal-mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { silent = true })
 
 -- vim: ts=2 sts=2 sw=2 et
