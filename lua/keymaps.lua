e7440403423b607ded9bc95af406acf8ddd968e5
Jon Glueckstein <jglueckstein@gmail.com>
2024-03-09 12:34:42 -0500

    added comments to make clearer which parts of the file I added

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,22 +11,22 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- repurpose the q key for closing a window
+-- jcg repurpose the q key for closing a window
 vim.keymap.set('n', 'Q', 'q', { silent = true, desc = 'start recording a macro' }) -- Q instead of q to start recording a macro
 vim.keymap.set('n', 'gQ', '@q', { silent = true, desc = 'run a macro called "q"' }) -- gQ instead of @q to run a macro called "q"
 vim.keymap.set('n', 'q', ':pclose!<CR>:quit<CR>', { silent = true, desc = 'fast quitting current window' }) -- q provides fast quitting of window (and nvim, if last window)
 
--- fast saving from all modes
+-- jcg fast saving from all modes
 vim.keymap.set('n', '<Leader>w', ':write<CR>', { silent = true, desc = 'Save buffer' }) -- normal mode
 vim.keymap.set('x', '<Leader>w', '<Esc>:write<CR>', { silent = true, desc = 'Save buffer' }) -- visual mode
 vim.keymap.set('n', '<C-s>', ':<C-u>write<CR>', { silent = true, desc = 'Save buffer' }) -- normal mode
 vim.keymap.set('x', '<C-s>', ':<C-u>write<CR>', { silent = true, desc = 'Save buffer' }) -- visual mode
 vim.keymap.set('c', '<C-s>', '<C-u>write<CR>', { silent = true, desc = 'Save buffer' }) -- command-line mode
 
--- fast save and quit all windows with CTRL-q
+-- jcg fast save and quit all windows with CTRL-q
 vim.keymap.set('n', '<C-q>', '<CMD>wqa<CR>', { silent = true, desc = 'save and quit all windows' })
 
--- fast exit from insert mode
+-- jcg fast exit from insert mode
 vim.keymap.set('i', 'jk', '<ESC>', { silent = true, desc = 'fast exit from insert mode' })
 vim.keymap.set('i', 'kj', '<ESC>', { silent = true, desc = 'fast exit from insert mode' })
 
@@ -67,7 +67,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- telescope picker for todo-comments
+-- jcg telescope picker for todo-comments
 vim.keymap.set('n', '<leader>st', '<cmd>TodoTelescope<CR>', { desc = '[S]earch [T]odo comments in Telescope' })
 
 -- vim: ts=2 sts=2 sw=2 et
