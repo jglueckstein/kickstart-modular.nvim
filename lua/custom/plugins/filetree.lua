c920693e4b02ab56655ac00e5370d20afadcd5e6
Jon Glueckstein <jglueckstein@gmail.com>
2024-03-09 17:05:26 -0500

    Attempting to configure some keybindings for Neotree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,15 +1,28 @@
 -- Unless you are still migrating, remove the deprecated commands from v1.x
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
   },
-  config = function ()
-    require('neo-tree').setup {}
+  config = function()
+    require('neo-tree').setup {
+      keys = {
+        {
+          '<leader>tf',
+          function()
+            require('neo-tree.command').execute {
+              toggle = true,
+              position = 'right',
+            }
+          end,
+          desc = 'neo-[T]ree toggle [F]ile tree',
+        },
+      },
+    }
   end,
 }
