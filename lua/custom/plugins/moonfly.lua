bf95e5f9b014bc2f5ea7b9d405266fd4ecc6b5e3
Jon Glueckstein <jglueckstein@gmail.com>
2024-03-11 08:55:02 -0400

    use init for moonfly colorscheme

--- lua/custom/plugins/moonfly.lua
+++ lua/custom/plugins/moonfly.lua
@@ -7,9 +7,8 @@ return {
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'bluz71/vim-moonfly-colors',
     name = 'moonfly',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
+    init = function()
       -- whether to color the cursor or not
       vim.g.moonflyCursorColor = true
 
