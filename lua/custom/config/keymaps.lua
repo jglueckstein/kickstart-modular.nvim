7b153e5b341f22c9aa0c45b44020ba53b2ce60c8
Jon Glueckstein <jglueckstein@gmail.com>
2024-04-04 11:16:31 -0500

    Moved my custom mappings from keymaps.lua to custom/config/keymaps.lua

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -2,6 +2,31 @@
 
 local map = vim.keymap.set
 
+-- repurpose the q key for closing a window
+-- Q instead of q to start recording a macro
+map('n', 'Q', 'q', { silent = true, desc = 'start recording a macro' })
+-- gQ instead of @q to run a macro called "q"
+map('n', 'gQ', '@q', { silent = true, desc = 'run a macro called "q"' })
+-- q provides fast quitting of window (and nvim, if last window)
+map('n', 'q', ':pclose!<CR>:quit<CR>', { silent = true, desc = 'fast quitting current window' })
+
+-- fast saving from all modes
+map('n', '<Leader>w', ':write<CR>', { silent = true, desc = 'Save buffer' })
+map('x', '<Leader>w', '<Esc>:write<CR>', { silent = true, desc = 'Save buffer' })
+map('n', '<C-s>', ':<C-u>write<CR>', { silent = true, desc = 'Save buffer' })
+map('x', '<C-s>', ':<C-u>write<CR>', { silent = true, desc = 'Save buffer' })
+map('c', '<C-s>', '<C-u>write<CR>', { silent = true, desc = 'Save buffer' })
+
+-- fast save and quit all windows with CTRL-q
+map('n', '<C-q>', '<CMD>wqa<CR>', { silent = true, desc = 'save and quit all windows' })
+
+-- fast exit from insert mode
+map('i', 'jk', '<ESC>', { silent = true, desc = 'fast exit from insert mode' })
+map('i', 'kj', '<ESC>', { silent = true, desc = 'fast exit from insert mode' })
+
+-- telescope picker for todo-comments
+map('n', '<leader>st', '<cmd>TodoTelescope<CR>', { desc = '[S]earch [T]odo comments in Telescope' })
+
 -- Config keymaps
 map('n', '<Leader>Ce', '<CMD>e $MYVIMRC<CR>', { desc = 'open [C]onfig file for [E]diting' })
 map('n', '<Leader>Cl', '<CMD>Lazy<CR>', { desc = '[C]onfig open [L]azy' })
@@ -27,4 +52,4 @@ map('n', '<A-}>', '<CMD>+tabmove<CR>', { desc = 'Move Tab Forward' })
 map('n', 'st', '<CMD>tabnew<CR>', { desc = 'Window [S]plit: new [T]ab' })
 
 -- Dev keymaps
-map('n', '<Leader><Leader>x', '<CMD>source %<CR>', { desc = 'Source This File' })
+map('n', '<LocalLeader>x', '<CMD>source %<CR>', { desc = 'Source This File' })
